152dfc71bc56795044f44fef57c17dffcc4cd558
Brian Lambert <brianlambert2408@gmail.com>
2023-01-26 11:21:39 +1100

    Completed tasks

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -8,7 +8,38 @@ class LinkedList
     @tail = nil
   end
 
+
   def reverse!
     # your code here
+    return @head if @head == nil || @head.next_node == nil
+
+    current_node = @head
+
+    @head = @tail
+    @tail = current_node
+
+    @head = reverse_node current_node
+
   end
+
+  private
+
+  def reverse_node node
+
+    if node == nil
+      return nil
+    end
+
+    n = node.next_node
+    node.next_node = node.prev_node
+    node.prev_node = n
+
+    if node.prev_node == nil
+      return node
+    end
+
+    return reverse_node n
+
+  end
+
 end
